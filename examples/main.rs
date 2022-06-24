18896ca811890fe767703fc69bcbbe118c29339f
gcxfd <i@rmw.link>
2022-06-24 18:05:32 +0800

    🔶

--- examples/main.rs
+++ examples/main.rs
@@ -6,10 +6,11 @@ fn main() -> Result<()> {
   for i in 1..=10 {
     let (word, mut img) = unsafe { captcha() };
     let word = unsafe { std::str::from_utf8_unchecked(&word) };
-    println!("{}", word);
     let exe = current_exe()?;
     let gif_path = exe.parent().unwrap().join(format!("{}.gif", i));
-    println!("{}", gif_path.display());
+
+    println!("{} {}", word, gif_path.display());
+
     let mut out = File::create(gif_path)?;
 
     let mut encoder = gif::Encoder::new(&mut out, WIDTH, HEIGHT, &[0, 0, 0, 0xFF, 0xFF, 0xFF])?;
