5c25633ac84225cca7f7f927cf5de95b8a904ac8
gcxfd <i@rmw.link>
2022-06-24 18:19:50 +0800

    .

--- src/c.rs
+++ src/c.rs
@@ -1,4 +1,28 @@
 #![allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
+/*
+   http://brokestream.com/captcha.html
+
+   Copyright (C) 2009 Ivan Tikhonov
+
+   This software is provided 'as-is', without any express or implied
+   warranty.  In no event will the authors be held liable for any damages
+   arising from the use of this software.
+
+   Permission is granted to anyone to use this software for any purpose,
+   including commercial applications, and to alter it and redistribute it
+   freely, subject to the following restrictions:
+
+   1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+   2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+   3. This notice may not be removed or altered from any source distribution.
+
+   Ivan Tikhonov, kefeer@brokestream.com
+
+*/
 
 pub const WIDTH: u16 = 200;
 pub const HEIGHT: u16 = 70;
@@ -609,27 +633,3 @@ static mut lt: [*mut i8; 25] = unsafe {
     lt24.as_ptr() as *mut _,
   ]
 };
-/*
-   http://brokestream.com/captcha.html
-
-   Copyright (C) 2009 Ivan Tikhonov
-
-   This software is provided 'as-is', without any express or implied
-   warranty.  In no event will the authors be held liable for any damages
-   arising from the use of this software.
-
-   Permission is granted to anyone to use this software for any purpose,
-   including commercial applications, and to alter it and redistribute it
-   freely, subject to the following restrictions:
-
-   1. The origin of this software must not be misrepresented; you must not
-   claim that you wrote the original software. If you use this software
-   in a product, an acknowledgment in the product documentation would be
-   appreciated but is not required.
-   2. Altered source versions must be plainly marked as such, and must not be
-   misrepresented as being the original software.
-   3. This notice may not be removed or altered from any source distribution.
-
-   Ivan Tikhonov, kefeer@brokestream.com
-
-*/
