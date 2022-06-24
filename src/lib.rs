6c2877c64e5231869e9898a75d58ca730b6e79b5
gcxfd <i@rmw.link>
2022-06-24 18:14:58 +0800

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,4 @@
-#![allow(
-  mutable_transmutes,
-  non_camel_case_types,
-  non_snake_case,
-  non_upper_case_globals,
-  unused_assignments,
-  unused_mut
-)]
+#![allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
 
 pub const WIDTH: u16 = 200;
 pub const HEIGHT: u16 = 70;
